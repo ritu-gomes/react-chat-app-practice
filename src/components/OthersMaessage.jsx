a08ed69b503d24dc852e57e0d2f58809567dc4ae
ritu-gomes <irengomes018@gmail.com>
2021-08-04 01:23:08 +0600

    app-created

---
+++ src/components/OthersMaessage.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const OthersMaessage = ({ lastMessage, message }) => {
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
+    return (
+        <div className="message-row">
+            {isFirstMessageByUser && (
+                <div
+                    className="message-avatar"
+                    style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+                />
+            )}
+            {
+                (message.attachments?.length > 0)
+                    ? (
+                        <img
+                            src={message.attachment[0].file}
+                            alt="attachment"
+                            className="message-image"
+                            style={{ marginLeft: isFirstMessageByUser ? "4px" : "48px" }}
+                        />
+                    ) :
+                    (
+                        <div className="message" style={{
+                            float: "left", backgroundColor: "#CABCDC",
+                            marginLeft: isFirstMessageByUser ? "4px" : "48px"
+                        }}>
+                            {message.text}
+
+                        </div>
+                    )
+            }
+        </div>
+    );
+};
+
+export default OthersMaessage;
